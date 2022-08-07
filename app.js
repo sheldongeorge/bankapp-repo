4f17bb7c7462eb1892971476fe5a70daa3c99036
Sheldon George <sheldon.george@outlook.com>
2022-08-06 18:24:35 -0700

    updated app js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-let mainButton = document.getElementById('myBtn');
-
-mainButton.addEventListener('click', (event) => {
-  let x = 3;
-  let b = 4;
-  let total = x + b;
-  console.log(total);
-});
-
-//Adding a new feature to this page
+let x = 0;
