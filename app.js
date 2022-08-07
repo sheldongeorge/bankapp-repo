94aeaa8b6d10d5250b1e43e4a36def1779f1b1f2
Sheldon George <sheldon.george@outlook.com>
2022-08-06 17:12:38 -0700

    adding a feature to app js

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let mainButton = document.getElementById('myBtn');
 mainButton.addEventListener('click', (event) => {
   console.log("First line of Javascript");
 });
+
+//Adding a new feature to this page
