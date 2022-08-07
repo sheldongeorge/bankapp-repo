8e926bd36c9f2470a791dbfdc4e295f4d2a7cc97
Lucas Stibbs <lucas.stibbs@gmail.com>
2022-08-07 01:53:45 -0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 let myButton = document.getElementById('myBtn');
 let myText = document.getElementById('myTxt');
+let mySpanny = document.getElementById('textSpan');
+let mySpanny2 = document.getElementById('mySpan');
+let increaseButton = document.getElementById('increaseBtn');
+let decreaseButton = document.getElementById('decreaseBtn');
+let myBalance = 300000;
 
-myButton.addEventListener('click', (event) => {
+function addUserName() {
   document.location.href = 'main.html';
+};
+
+mySpanny2.innerHTML = myBalance;
+
+increaseButton.addEventListener('click', (event) => {
+  myBalance += 5000;
+  mySpanny2.innerHTML = myBalance;
+});
+
+decreaseButton.addEventListener('click', (event) => {
+  myBalance -= 5000;
+  mySpanny2.innerHTML = myBalance;
 });
