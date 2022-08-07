0a5a67e7e8289e5f783292d9fa9f0e35b8244301
Sheldon George <sheldon.george@outlook.com>
2022-08-06 17:18:38 -0700

    changed button function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let mainButton = document.getElementById('myBtn');
 
 mainButton.addEventListener('click', (event) => {
-  console.log("First line of Javascript");
+  let x = 3;
+  let b = 4;
+  let total = x + b;
+  console.log(total);
 });
 
 //Adding a new feature to this page
