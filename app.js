f9e3ce58a4f5ece8fb193b1b7be0117a4831eb98
Lucas Stibbs <lucas.stibbs@gmail.com>
2022-08-14 00:18:57 -0700

    added new features to login screen

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 let main_header = document.getElementById('mainHeader');
 let main_span = document.getElementById('mainSpan');
+let welcome_id = document.getElementById('welcomeSpan');
+let user_first = document.getElementById('userFirst');
+let user_pass = document.getElementById('userPass');
+let balance_span = document.getElementById('balanceSpan');
+let person = {
+  name: "",
+  balance: "30,000",
+  isAdmin: true,
+};
 
 main_header.addEventListener('mouseover', (event) => {
-  main_span.style.color = 'darkolivegreen';
-  main_span.style.transition = '0.4s ease';
+  main_span.style.color = 'white';
+  main_span.style.transition = '0.6s ease';
 });
 
 main_header.addEventListener('mouseleave', (event) => {
   main_span.style.color = 'black';
 });
 
-function changePage() {
-  window.location.href = "main.html";
+function assignName() {
+  if(user_pass.value != 'pass')
+  {
+    alert('Please enter a valid password.');
+  }else {
+    window.location.href = 'main.html';
+  }
+  let userId = user_first.value;
+  person.name = userId;
+  welcome_id.innerText = person.name;
 };
+
+balance_span.innerText = person.balance;
+
